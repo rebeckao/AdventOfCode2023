3ddc56e4f4a4215f9da18085fcc28013f3a431ff
rebeckao <rebecka.oefverholm@gmail.com>
2023-12-08 12:54:14 +0100

    Day 07 part 2

--- src/day07/Day07.java
+++ src/day07/Day07.java
@@ -1,6 +1,7 @@
 package day07;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.stream.Collectors.toMap;
@@ -11,6 +12,19 @@ public class Day07 {
                 .map(it -> it.split(" "))
                 .map(it -> new HandBid(it[0], Integer.parseInt(it[1])))
                 .sorted((a, b) -> compare(a.hand, b.hand))
+                .toList();
+        int winnings = 0;
+        for (int i = 0; i < handBids.size(); i++) {
+            winnings += (i + 1) * handBids.get(i).bid;
+        }
+        return winnings;
+    }
+
+    static int totalWinningsUsingJokerRule(List<String> hands) {
+        List<HandBid> handBids = hands.stream()
+                .map(it -> it.split(" "))
+                .map(it -> new HandBid(it[0], Integer.parseInt(it[1])))
+                .sorted((a, b) -> compareUsingJokerRule(a.hand, b.hand))
                 .toList();
         int winnings = 0;
         for (int i = 0; i < handBids.size(); i++) {
@@ -39,21 +53,41 @@ public class Day07 {
         return 0;
     }
 
+    private static int compareUsingJokerRule(String hand1, String hand2) {
+        int strength1 = handStrengthUsingJokerRule(hand1);
+        int strength2 = handStrengthUsingJokerRule(hand2);
+        if (strength1 > strength2) {
+            return 1;
+        }
+        if (strength2 > strength1) {
+            return -1;
+        }
+        for (int i = 0; i < hand1.length(); i++) {
+            int card1 = cardStrengthUsingJokerRule(hand1.charAt(i));
+            int card2 = cardStrengthUsingJokerRule(hand2.charAt(i));
+            if (card1 == card2) {
+                continue;
+            }
+            return card1 > card2 ? 1 : -1;
+        }
+        return 0;
+    }
+
     private static int handStrength(String hand) {
-        Collection<Integer> occurences = hand.chars().boxed().collect(toMap(it -> it, _ -> 1, (existing, _) -> existing + 1)).values();
-        if (occurences.size() == 1) {
+        Collection<Integer> occurrences = hand.chars().boxed().collect(toMap(it -> it, _ -> 1, (existing, _) -> existing + 1)).values();
+        if (occurrences.size() == 1) {
             return 7;
         }
-        if (occurences.size() == 2) {
-            if (occurences.contains(4)) {
+        if (occurrences.size() == 2) {
+            if (occurrences.contains(4)) {
                 return 6;
             }
             return 5;
         }
-        if (occurences.contains(3)) {
+        if (occurrences.contains(3)) {
             return 4;
         }
-        long numberOfPairs = occurences.stream().filter(it -> it == 2).count();
+        long numberOfPairs = occurrences.stream().filter(it -> it == 2).count();
         if (numberOfPairs == 2) {
             return 3;
         }
@@ -63,6 +97,42 @@ public class Day07 {
         return 1;
     }
 
+    protected static int handStrengthUsingJokerRule(String rawHand) {
+        int numberOfJokers = (int) rawHand.chars().filter(it -> it == 'J').count();
+        if (numberOfJokers == 5) {
+            return 7; // five of a kind
+        }
+        String hand = rawHand.replace("J", "");
+        Collection<Integer> occurrences = hand.chars()
+                .boxed()
+                .collect(toMap(it -> it, _ -> 1, (existing, _) -> existing + 1))
+                .values();
+        int maxOccurrencesIncludingJokers = Collections.max(occurrences) + numberOfJokers;
+        if (maxOccurrencesIncludingJokers == 5) {
+            return 7; // five of a kind
+        }
+        if (maxOccurrencesIncludingJokers == 4) {
+            return 6; // four of a kind
+        }
+        if (occurrences.size() == 2 && maxOccurrencesIncludingJokers == 3) {
+            return 5; // full house
+        }
+        if (maxOccurrencesIncludingJokers == 3) {
+            return 4; // three of a kind
+        }
+        long numberOfPairs = occurrences.stream().filter(it -> it == 2).count();
+        if (numberOfPairs == 2) {
+            return 3; // two pair
+        }
+        if (numberOfPairs == 1) {
+            return 2; // one pair
+        }
+        if (maxOccurrencesIncludingJokers == 2) {
+            return 2; // one pair
+        }
+        return 1; // high card
+    }
+
     private static int cardStrength(char character) {
         return switch (character) {
             case 'A' -> 14;
@@ -70,6 +140,17 @@ public class Day07 {
             case 'Q' -> 12;
             case 'J' -> 11;
             case 'T' -> 10;
+            default -> character - '0';
+        };
+    }
+
+    private static int cardStrengthUsingJokerRule(char character) {
+        return switch (character) {
+            case 'A' -> 13;
+            case 'K' -> 12;
+            case 'Q' -> 11;
+            case 'T' -> 10;
+            case 'J' -> 1;
             default -> character - '0';
         };
     }
--- src/day07/Day07Test.java
+++ src/day07/Day07Test.java
@@ -1,6 +1,8 @@
 package day07;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -23,5 +25,40 @@ class Day07Test {
     @Test
     void totalWinningsRealData() {
         assertEquals(246795406, Day07.totalWinnings(realData(7)));
+    }
+
+    @Test
+    void totalWinningsJokerRule() {
+        assertEquals(5905, Day07.totalWinningsUsingJokerRule(List.of(
+                "32T3K 765",
+                "T55J5 684",
+                "KK677 28",
+                "KTJJT 220",
+                "QQQJA 483"
+        )));
+    }
+
+    @Test
+    void totalWinningsJokerRuleRealData() {
+        assertEquals(249356515, Day07.totalWinningsUsingJokerRule(realData(7)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1234J, 2",
+            "1233J, 4",
+            "1222J, 6",
+            "1111J, 7",
+            "1122J, 5",
+            "123JJ, 4",
+            "122JJ, 6",
+            "111JJ, 7",
+            "12JJJ, 6",
+            "11JJJ, 7",
+            "1JJJJ, 7",
+            "JJJJJ, 7"
+    })
+    void handStrengthUsingJokerRule(String hand, int expectedStrength) {
+        assertEquals(expectedStrength, Day07.handStrengthUsingJokerRule(hand));
     }
 }
