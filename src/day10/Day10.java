1eb36349853e41f5b8a3c2a2bd155d83049c3bad
rebeckao <rebecka.oefverholm@gmail.com>
2023-12-21 18:10:32 +0100

    Day 10 part 2

--- src/day10/Day10.java
+++ src/day10/Day10.java
@@ -1,5 +1,7 @@
 package day10;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -14,10 +16,130 @@ public class Day10 {
             if (currentPipeShape == 'S') {
                 return visitedPipes / 2;
             }
-            Position newPosition = nextPosition(pipeMap, currentPipeShape, currentPosition, lastPosition);
+            Position newPosition = nextPosition(currentPipeShape, currentPosition, lastPosition);
             lastPosition = currentPosition;
             currentPosition = newPosition;
         }
+    }
+
+    public static int tilesInLoop(List<String> pipeMap) {
+        Position startPosition = findStartPosition(pipeMap);
+        Set<Position> loopPositions = findLoopPositions(pipeMap, startPosition);
+        pipeMap = removeStartMarker(pipeMap, startPosition);
+        int enclosedTiles = 0;
+        for (int row = 0; row < pipeMap.size(); row++) {
+            Enclosed enclosed = Enclosed.NO;
+            String currentRow = pipeMap.get(row);
+            for (int col = 0; col < currentRow.length(); col++) {
+                if (loopPositions.contains(new Position(row, col))) {
+                    char currentShape = currentRow.charAt(col);
+                    if (enclosed == Enclosed.YES) {
+                        if (currentShape == '|') {
+                            enclosed = Enclosed.NO;
+                        } else if (currentShape == 'L') {
+                            enclosed = Enclosed.BELOW;
+                        } else if (currentShape == 'F') {
+                            enclosed = Enclosed.ABOVE;
+                        }
+                    } else if (enclosed == Enclosed.NO) {
+                        if (currentShape == '|') {
+                            enclosed = Enclosed.YES;
+                        } else if (currentShape == 'L') {
+                            enclosed = Enclosed.ABOVE;
+                        } else if (currentShape == 'F') {
+                            enclosed = Enclosed.BELOW;
+                        }
+                    } else if (enclosed == Enclosed.ABOVE) {
+                        if (currentShape == 'J') {
+                            enclosed = Enclosed.NO;
+                        } else if (currentShape == '7') {
+                            enclosed = Enclosed.YES;
+                        }
+                    } else if (enclosed == Enclosed.BELOW) {
+                        if (currentShape == 'J') {
+                            enclosed = Enclosed.YES;
+                        } else if (currentShape == '7') {
+                            enclosed = Enclosed.NO;
+                        }
+                    }
+                } else if (enclosed == Enclosed.YES) {
+                    enclosedTiles++;
+                }
+            }
+        }
+        return enclosedTiles;
+    }
+
+    private static List<String> removeStartMarker(List<String> pipeMap, Position startPosition) {
+        Character startShape = findStartShape(pipeMap, startPosition);
+        String fixedRow = pipeMap.get(startPosition.row).replace('S', startShape);
+        List<String> newMap = new ArrayList<>(pipeMap);
+        newMap.set(startPosition.row, fixedRow);
+        return newMap;
+    }
+
+    private static Character findStartShape(List<String> pipeMap, Position startPosition) {
+        boolean pointsNorth = pointsNorth(pipeMap, startPosition);
+        boolean pointsSouth = pointsSouth(pipeMap, startPosition);
+        boolean pointsWest = pointsWest(pipeMap, startPosition);
+        if (pointsNorth) {
+            if (pointsSouth) {
+                return '|';
+            } else if (pointsWest) {
+                return 'J';
+            } else {
+                return 'L';
+            }
+        } else if (pointsSouth) {
+            if (pointsWest) {
+                return '7';
+            } else {
+                return 'F';
+            }
+        } else {
+            return '-';
+        }
+    }
+
+    private static boolean pointsEast(List<String> pipeMap, Position startPosition) {
+        return startPosition.column < pipeMap.get(0).length() && Set.of('-', 'J', '7').contains(pipeMap.get(startPosition.row).charAt(startPosition.column + 1));
+    }
+
+    private static boolean pointsWest(List<String> pipeMap, Position startPosition) {
+        return startPosition.column > 0 && Set.of('-', 'F', 'L').contains(pipeMap.get(startPosition.row).charAt(startPosition.column - 1));
+    }
+
+    private static boolean pointsSouth(List<String> pipeMap, Position startPosition) {
+        return startPosition.row < pipeMap.size() && Set.of('|', 'J', 'L').contains(pipeMap.get(startPosition.row + 1).charAt(startPosition.column));
+    }
+
+    private static boolean pointsNorth(List<String> pipeMap, Position startPosition) {
+        return startPosition.row > 0 && Set.of('|', 'F', '7').contains(pipeMap.get(startPosition.row - 1).charAt(startPosition.column));
+    }
+
+    private enum Enclosed {
+        NO,
+        ABOVE,
+        BELOW,
+        YES
+    }
+
+    private static Set<Position> findLoopPositions(List<String> pipeMap, Position startPosition) {
+        Position lastPosition = startPosition;
+        Set<Position> loopPositions = new HashSet<>();
+        loopPositions.add(lastPosition);
+        Position currentPosition = findFirstStep(pipeMap, lastPosition);
+        while (true) {
+            loopPositions.add(currentPosition);
+            char currentPipeShape = pipeMap.get(currentPosition.row).charAt(currentPosition.column);
+            if (currentPipeShape == 'S') {
+                break;
+            }
+            Position newPosition = nextPosition(currentPipeShape, currentPosition, lastPosition);
+            lastPosition = currentPosition;
+            currentPosition = newPosition;
+        }
+        return loopPositions;
     }
 
     private static Position findStartPosition(List<String> pipeMap) {
@@ -31,22 +153,22 @@ public class Day10 {
     }
 
     private static Position findFirstStep(List<String> pipeMap, Position startPosition) {
-        if (startPosition.row > 0 && Set.of('|', 'F', '7').contains(pipeMap.get(startPosition.row - 1).charAt(startPosition.column))) {
+        if (pointsNorth(pipeMap, startPosition)) {
             return new Position(startPosition.row - 1, startPosition.column);
         }
-        if (startPosition.row < pipeMap.size() && Set.of('|', 'J', 'L').contains(pipeMap.get(startPosition.row + 1).charAt(startPosition.column))) {
+        if (pointsSouth(pipeMap, startPosition)) {
             return new Position(startPosition.row + 1, startPosition.column);
         }
-        if (startPosition.column > 0 && Set.of('-', 'F', 'L').contains(pipeMap.get(startPosition.row).charAt(startPosition.column - 1))) {
+        if (pointsWest(pipeMap, startPosition)) {
             return new Position(startPosition.row, startPosition.column - 1);
         }
-        if (startPosition.column < pipeMap.get(0).length() && Set.of('-', 'J', '7').contains(pipeMap.get(startPosition.row).charAt(startPosition.column + 1))) {
+        if (pointsEast(pipeMap, startPosition)) {
             return new Position(startPosition.row, startPosition.column + 1);
         }
         throw new IllegalStateException();
     }
 
-    private static Position nextPosition(List<String> pipeMap, char currentPipeShape, Position currentPosition, Position lastPosition) {
+    private static Position nextPosition(char currentPipeShape, Position currentPosition, Position lastPosition) {
         Position north = new Position(currentPosition.row - 1, currentPosition.column);
         Position south = new Position(currentPosition.row + 1, currentPosition.column);
         Position west = new Position(currentPosition.row, currentPosition.column - 1);
--- src/day10/Day10Test.java
+++ src/day10/Day10Test.java
@@ -1,7 +1,10 @@
 package day10;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static common.Common.realData;
@@ -30,5 +33,62 @@ class Day10Test {
     @Test
     void stepsToFarthestPipeRealData() {
         assertEquals(7097, Day10.stepsToFarthestPipe(realData(10)));
+    }
+
+    @ParameterizedTest()
+    @CsvSource({
+            "1," +
+            ".....n" +
+            ".S-7.n" +
+            ".|.|.n" +
+            ".L-J.n" +
+            ".....",
+            "1," +
+            "..F7.n" +
+            ".FJ|.n" +
+            "SJ.L7n" +
+            "|F--Jn" +
+            "LJ...",
+            "4," +
+            "...........n" +
+            ".S-------7.n" +
+            ".|F-----7|.n" +
+            ".||.....||.n" +
+            ".||.....||.n" +
+            ".|L-7.F-J|.n" +
+            ".|..|.|..|.n" +
+            ".L--J.L--J.n" +
+            "...........",
+            "8," +
+            ".F----7F7F7F7F-7....n" +
+            ".|F--7||||||||FJ....n" +
+            ".||.FJ||||||||L7....n" +
+            "FJL7L7LJLJ||LJ.L-7..n" +
+            "L--J.L7...LJS7F-7L7.n" +
+            "....F-J..F7FJ|L7L7L7n" +
+            "....L7.F7||L7|.L7L7|n" +
+            ".....|FJLJ|FJ|F7|.LJn" +
+            "....FJL-7.||.||||...n" +
+            "....L---J.LJ.LJLJ...",
+            "10," +
+            "FF7FSF7F7F7F7F7F---7n" +
+            "L|LJ||||||||||||F--Jn" +
+            "FL-7LJLJ||||||LJL-77n" +
+            "F--JF--7||LJLJ7F7FJ-n" +
+            "L---JF-JLJ.||-FJLJJ7n" +
+            "|F|F-JF---7F7-L7L|7|n" +
+            "|FFJF7L7F-JF7|JL---7n" +
+            "7-L-JL7||F7|L7F-7F7|n" +
+            "L.L7LFJ|||||FJL7||LJn" +
+            "L7JLJL-JLJLJL--JLJ.L"
+    })
+    void tilesInLoop(int expected, String map) {
+        List<String> pipeMap = Arrays.stream(map.split("n")).toList();
+        assertEquals(expected, Day10.tilesInLoop(pipeMap));
+    }
+
+    @Test
+    void tilesInLoopRealData() {
+        assertEquals(355, Day10.tilesInLoop(realData(10)));
     }
 }
